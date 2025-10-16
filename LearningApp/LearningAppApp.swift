2be42cb789210f6c8dbebed0e41ff61ebe448739
Ghadeer Fallatah <ghadeer1781@gmail.com>
2025-10-16 12:02:34 +0300

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Ghadeer Fallatah on 24/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
