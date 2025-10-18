6a2354761b481f1ef3ab0394e11db855433a7002
Ghadeer Fallatah <ghadeer1781@gmail.com>
2025-10-18 19:21:53 +0300

    view changes

---
+++ LearningApp/View/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  LearningApp
+//
+//  Created by Ghadeer Fallatah on 24/04/1447 AH.
+//
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+
+    }
+}
+
+#Preview {
+    ContentView()
+}
